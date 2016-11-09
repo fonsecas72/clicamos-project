f18b9beca5269fae3dea0a3a0b5af1349587115f
Hugo Fonseca <hugo.fonseca@beubi.com>
2016-11-09 19:03:26 +0000

    fix

--- iosocket.js
+++ iosocket.js
@@ -11,6 +11,7 @@ app.get('/', function (req, res) {
 });
 
 io.on('connection', function (socket) {
+    io.emit('user clicks', clicks);
     socket.on('user clicks', function () {
         clicks++;
         io.emit('user clicks', clicks);
